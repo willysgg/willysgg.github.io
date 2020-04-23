dc33b75a6e4ca3a1067568908450e2ef6d79e418
willysgg <63767570+willysgg@users.noreply.github.com>
2020-04-23 08:01:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,4 +12,19 @@ self.addEventListener('install', function(event) {
   );
   return self.clients.claim();
 });
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('first-app')
+      .then(function(cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/css/app.css',
+          '/src/js/app.js'
+        ])
+      })
+  );
+  return self.clients.claim();
+});
+
 
